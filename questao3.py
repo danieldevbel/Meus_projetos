6ce6f6c3092ad0a4f467306b3be0626da6e4ef88
danieljrhemp <127910407+danieljrhemp@users.noreply.github.com>
2023-05-25 20:15:04 -0300

    Delete questao3.py

--- questao3.py
+++
@@ -1,9 +0,0 @@
-#questao3
-"""Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos."""
-
-def soma(n1,n2,n3):
-    somas = n1 + n2 + n3
-    return somas
-
-somas=soma(5,60,5)
-print(f'A soma dos valores é igual a {somas}')
