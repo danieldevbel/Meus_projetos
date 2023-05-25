e6718e798c0a0c5db0e41d93780906fa1ad7aa61
danieljrhemp <127910407+danieljrhemp@users.noreply.github.com>
2023-05-25 20:13:50 -0300

    Create questao3.py

---
+++ questao3.py
@@ -0,0 +1,9 @@
+#questao3
+"""Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos."""
+
+def soma(n1,n2,n3):
+    somas = n1 + n2 + n3
+    return somas
+
+somas=soma(5,60,5)
+print(f'A soma dos valores é igual a {somas}')
